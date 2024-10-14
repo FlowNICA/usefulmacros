d4dbb5fa8766afbb8fa69acdd335428c515acee9
PeterParfenov <terrylapard@gmail.com>
2024-10-14 14:23:58 +0300

    Added simple Tpass calculator

--- Tpass.C
+++ Tpass.C
@@ -1,6 +1,6 @@
-void Tpass(float sqrtSNN, float R)
+void Tpass(float sqrtSNN, float R1, float R2)
 {
     float mp = 0.938;
-    float t_pass = 4*mp*R/sqrt(sqrtSNN*sqrtSNN - 4*mp*mp);
+    float t_pass = 2*mp*(R1+R2)/sqrt(sqrtSNN*sqrtSNN - 4*mp*mp);
     cout << "Tpass = " << t_pass << " fm/c" << endl;
 }
